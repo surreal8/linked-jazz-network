d601640025314124485daa8a14be90909658dd12
surreal8 <tinashah8@gmail.com>
2015-06-26 17:14:28 -0500

    adding browser update script

--- head.php
+++ head.php
@@ -21,3 +21,14 @@
   <link rel="stylesheet" href="css/colorbox.css">
 
   <link rel="shortcut icon" href="menu/aictheme_favicon.png" type="image/x-icon">
+  
+  <script> 
+	var $buoop = {c:2}; 
+	function $buo_f(){ 
+	 var e = document.createElement("script"); 
+	 e.src = "//browser-update.org/update.min.js"; 
+	 document.body.appendChild(e);
+	};
+	try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+	catch(e){window.attachEvent("onload", $buo_f)}
+  </script> 
