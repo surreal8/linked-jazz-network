727bc2bceca72115ec47284c73ba7f0f5ed04c23
surreal8 <tinashah8@gmail.com>
2015-06-26 10:25:19 -0500

    adding meta description

--- network.php
+++ network.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="James McNeill Whistler (1834–1903) and Theodore Roussel (1847–1903) lived and worked among a vibrant community of artists, models, printers, and literary figures, some of whom were members of their own families. This interactive program allows the user to explore the social and professional networks of these artists.">
 <title>Linked Visions</title>
 <script src="js/conf.js"></script> 
 <script src="js/jquery.min.js"></script>
