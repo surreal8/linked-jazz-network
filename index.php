d065347b21a9006fe57eaf879004c6b2039d0a5f
surreal8 <tinashah8@gmail.com>
2015-11-16 13:11:50 -0600

    removing bottom rule on landing page

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
             <div class="divider"><img src="menu/dash.png"/></div>
             <p>
             James McNeill Whistler (1834–1903) and Theodore Roussel (1847–1903) lived and worked among a vibrant community of artists, models, printers, and literary figures, some of whom were members of their own families. This interactive program allows the user to explore the social and professional networks of these artists.</p>
-            <div class="divider"><img src="menu/dash.png"/></div> 
             <a id="begin" href="network.php">BEGIN</a>
         </div>
         
